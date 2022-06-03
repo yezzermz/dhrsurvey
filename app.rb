dd99ff9df710dba7acd3d79751d75fa04952e06f
yezzermz <106792800+yezzermz@users.noreply.github.com>
2022-06-03 16:03:34 -0500

    Client Side Validation
    Javascript validation for user inputs:
    - Prevents null or invalid names or phone numbers to be submitted
    - Error message and input field color change to notify user where error is

--- app.rb
+++ app.rb
@@ -10,6 +10,9 @@ end
 
 # Creates database entries after user input
 post "/new_survey/create" do
+	#if user phone# already in database reject survey and inform user feedback had already been received
+	
+	#if user information not stored save and submit
 	redirect "/thanks"
 end
 

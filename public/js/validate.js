3ece57918436c8ba0b3ba3aec8ad58ec1fc76940
yezzermz <106792800+yezzermz@users.noreply.github.com>
2022-06-03 22:21:26 -0500

    code cleanup
    Javascript code cleanup
    - functions added for repeated code

--- public/js/validate.js
+++ public/js/validate.js
@@ -12,40 +12,60 @@ const error = document.getElementById("nameError");
 document.getElementById("submit").addEventListener("click", validate);
 
 function validate(e) {
+
+  // Makes error message visible
+  function alert(){
+    e.preventDefault();
+    error.classList.add("visible");
+    error.setAttribute("aria-hidden", false);
+    error.setAttribute("aria-invalid", true);
+  }
+
+  // Show error message
+  function paint(v) {
+    v.classList.add("invalid");     // add invalid css class to input for red border
+    v.value = "";                   // Wrong input reset field
+  }
+
+  // Check if radios are selected
+  function radios(x){
+    for (var i = 0, len = x.length; i < len; i++) {
+      if (!x[i].checked) {
+        alert();
+      }
+    }
+  }
+
+  function fields(y){
+    if (!y.value || !y.value.match(letters)) {
+      alert();
+      paint(y);
+    }
+  }
+
   first.classList.remove("invalid");    // On each submit click reset input field css class
   last.classList.remove("invalid");     //
   phone.classList.remove("invalid");    //
 
 
   // If first name is null or contains anything other than letters fail validation
-  if (!first.value || !first.value.match(letters)) {
-    e.preventDefault();                             // prevents from from subbmission
-    first.classList.add("invalid");               // add invalid css class to input for red border
-    error.classList.add("visible");               // Makes error message visible
-    error.setAttribute("aria-hidden", false);     //
-    error.setAttribute("aria-invalid", true);     //
-    first.value = "";                             // Wrong input reset field
-  }
+  fields(first);
 
   // If last name is null or contains anything other than letters fail validation
-  if (!last.value || !last.value.match(letters)) {
-    e.preventDefault();
-    last.classList.add("invalid");
-    error.classList.add("visible");
-    error.setAttribute("aria-hidden", false);
-    error.setAttribute("aria-invalid", true);
-    last.value = "";
-  }
+  fields(last);
 
   // If phone number is null or is not a proper number fail validation
   if (!phone.value || !phone.value.match(phoneno)) {
-    e.preventDefault();
-    phone.classList.add("invalid");
-    error.classList.add("visible");
-    error.setAttribute("aria-hidden", false);
-    error.setAttribute("aria-invalid", true);
-    phone.value = "";
+    alert();
+    paint(phone);
   }
+
+  // Check if radios are selected
+  radios(document.getElementsByName("rating1"));
+  radios(document.getElementsByName("rating2"));
+  radios(document.getElementsByName("rating3"));
+  radios(document.getElementsByName("rating4"));
+  radios(document.getElementsByName("rating5"));
 
   // else continue with form submission
   return;
